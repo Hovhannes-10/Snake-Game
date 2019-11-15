833daf4866ea000683a45508c26e57f286f0253b
Hovhannes-10 <hovodanielayan@gmail.com>
2019-11-15 06:01:59 +0400

    Updates

---
+++ precache-manifest.969c188f52c4a086cb09131b3ef0c666.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676f627c1bfa4a335e2006c823beaf2f",
+    "url": "/Snake-Game/index.html"
+  },
+  {
+    "revision": "b1a3d524b8a0f12c7e4f",
+    "url": "/Snake-Game/static/css/main.8bedccb7.chunk.css"
+  },
+  {
+    "revision": "b937e4131c7202143704",
+    "url": "/Snake-Game/static/js/2.383536bd.chunk.js"
+  },
+  {
+    "revision": "b1a3d524b8a0f12c7e4f",
+    "url": "/Snake-Game/static/js/main.fdc286d2.chunk.js"
+  },
+  {
+    "revision": "2b81578026605e5fab64",
+    "url": "/Snake-Game/static/js/runtime-main.7b72d2e0.js"
+  }
+]);
